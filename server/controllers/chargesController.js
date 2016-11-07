9f0ce19f2084e7bd55f22659fb1ace6d14eb51d0
James Byrne <jamesdesbyrne@gmail.com>
2016-11-07 16:21:39 +0000

    Adding the stripeHandlerModule
    This module will take care of functions such as creating a charge,
    customer or order. Its function is to allow easy reuse of stripe
    functionality throughout the application.

--- server/controllers/chargesController.js
+++ server/controllers/chargesController.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')('YourAPIKey');
+import stripeHandlerModule from '../modules/stripeHandlerModule';
 const chargesController = {};
 
 /**
@@ -7,30 +7,12 @@ const chargesController = {};
  * client. We then create a Stripe Charge using the token, for the given amount.
  */
 chargesController.handlePost = function (req, res, next) {
-  // Point the app at Testing Pays' Stripe charge Sim
-  stripe.setHost('api.testingpays.com', 443, 'https');
-  stripe._setApiField('basePath', '/stripe/v1/');
-  stripe._prepResources();
-
-  /**
-   * NOTE: Stripe and most other payment processors only accept amounts as a
-   * cent value. Below we multiply the amount by 100 and round the value.
-   *
-   * This approach is simple though perhaps slightly naieve. In a real
-   * application you may want to find a more robust method of carrying out the
-   * operation.
-   */
-
-  stripe.charges.create({
-    amount: Math.round(parseFloat(req.body.amount) * 100),
-    currency: 'eur',
-    source: req.body.stripeToken,
-    description: 'Charge for testing.pays@example.com'
-  }, function (err, charge) {
-    if (err) {
-      return res.send(err);
+  stripeHandlerModule.createCharge(req.body.amount, req.body.stripeToken)
+  .then((result) => {
+    if (result.error) {
+      res.status(422).send(result.error);
     } else {
-      return res.send(charge);
+      res.status(200).send(result);
     }
   });
 };
---
+++ server/modules/stripeHandlerModule.js
@@ -0,0 +1,65 @@
+const stripe = require('stripe')('YourAPIKeyHere');
+const stripeHandlerModule = {};
+
+// Point the app at Testing Pays' Stripe Charge sim
+stripe.setHost('0.0.0.0', 8000, 'http');
+stripe._setApiField('basePath', '/stripe/v1/');
+stripe._prepResources();
+
+/**
+ * Attempts to charge the user a given amount against a given source (usually a card)
+ * @method createCharge
+ * @param  float      amount      An amount to charge the user
+ * @param  string     source      The source (generally a card) to charge
+ * @return object     return      Returns the result of the stripe charge
+ *
+ * NOTE: Stripe and most other payment processors only accept amounts as a
+ * cent value. Below we multiply the amount by 100 and round the value.
+ *
+ * This approach is simple though perhaps slightly naieve. In a real
+ * application you may want to find a more robust method of carrying out the
+ * operation.
+ */
+stripeHandlerModule.createCharge = function (amount, source) {
+  return stripe.charges.create({
+    amount: Math.round(parseFloat(amount) * 100),
+    currency: 'usd',
+    source: source,
+    description: 'Charge for testing.pays@example.com'
+  }).then(
+    (res) => {
+      return res;
+    },
+    (err) => {
+      return this._stripeHandler(err);
+    }
+  );
+};
+
+/**
+ * Finds the type of error being returned
+ * @method _stripeHandler
+ * @param  object      err      The error returned from the stripe charge
+ * @return object      {}       Returns an object with an error message
+ */
+stripeHandlerModule._stripeHandler = function (err) {
+  switch (err.type) {
+    case ('RateLimitError'):            // Too many requests hit the API too quickly
+    case ('StripeAPIError'):            // Generic stripe error
+    case ('StripeCardError'):           // Most common error, occurs when card cannot be charged
+    case ('StripeConnectionError'):     // Failed to connect to stripes api
+    case ('StripeInvalidRequestError'): // The request has invalid params
+    case ('StripeAuthenticationError'): // Failed to authenticate with stripes api
+      if (err.raw) {
+        return { error: err.raw };
+      } else {
+        return { error: err.message };
+      }
+
+    default:
+      // Handle any other types of unexpected errors
+      return { error: 'err_not_stripe' };
+  }
+};
+
+export default stripeHandlerModule;
