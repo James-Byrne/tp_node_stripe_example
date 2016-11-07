2014403d8bb23b4b24fe3f2607a3e1f969ba6e2f
James Byrne <jamesdesbyrne@gmail.com>
2016-11-07 17:58:09 +0000

    Working on tests for the charges controller

---
+++ test/controllers/chargesControllerTest.js
@@ -0,0 +1,63 @@
+import { expect } from 'chai';
+import supertest from 'supertest-as-promised';
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import app from '../../server/app';
+
+describe('/charges', function () {
+  beforeEach(function () {});
+  afterEach(function () {});
+
+  describe('Make POST requests', function () {
+    it('should return success message', function () {
+      return supertest(app)
+      .post('/charges')
+      .send({
+        amount: 123.00,
+        stripeToken: 'tok_12345678'
+      }).expect(200).then((res) => {
+        expect(res.body.status).to.equal('success');
+      }).catch((err) => {
+        console.log(err);
+      });
+    });
+
+    it('should return invalid_request_error', function () {
+      return supertest(app)
+      .post('/charges')
+      .send({
+        amount: 123.80,
+        stripeToken: 'tok_12345678'
+      }).expect(422).then((res) => {
+        expect(res.body.type).to.equal('invalid_request_error');
+      }).catch((err) => {
+        console.log(err);
+      });
+    });
+
+    it('should return rate_limit_error', function () {
+      return supertest(app)
+      .post('/charges')
+      .send({
+        amount: 123.91,
+        stripeToken: 'tok_12345678'
+      }).expect(422).then((res) => {
+        expect(res.body.type).to.equal('rate_limit_error');
+      }).catch((err) => {
+        console.log(err);
+      });
+    });
+
+    it('should return authentication_error', function () {
+      return supertest(app)
+      .post('/charges')
+      .send({
+        amount: 123.86,
+        stripeToken: 'tok_12345678'
+      }).expect(422).then((res) => {
+        expect(res.body.type).to.equal('authentication_error');
+      }).catch((err) => {
+        console.log(err);
+      });
+    });
+  });
+});
